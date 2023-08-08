15bf5720dd36351aec3d4517306777ced29923e4
hoangviet10101 <hoangviet10101@gmail.com>
2023-08-08 09:26:57 +0700

    polish

--- Project/backend/src/data-login.ts
+++ Project/backend/src/data-login.ts
@@ -4,14 +4,16 @@ export const sample_users_login = [
         "id": "1",
         "email": "Viet123@gmail.com",
         "password": "123",
-        "isAdmin": true
+        "isAdmin": true,
+        "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IlZpZXQxMjNAZ21haWwuY29tIiwiaXNBZG1pbiI6dHJ1ZSwiaWF0IjoxNjkxNDYwNDcxLCJleHAiOjE2OTIzMjQ0NzF9.h3gbNZohC-Enye9Tn0sscaxLW8MbAjaFcv08jWtDJ8k"
     },
     {
         "name": "Quan",
         "id": "2",
         "email": "Quan12345@gmail.com",
         "password": "12345",
-        "isAdmin": false
+        "isAdmin": false,
+        "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IlF1YW4xMjM0NUBnbWFpbC5jb20iLCJpc0FkbWluIjpmYWxzZSwiaWF0IjoxNjkxNDYwMzk2LCJleHAiOjE2OTIzMjQzOTZ9.JJCfeMBnnvA7YSl37H6wkmPb20grOw4KHSr3HSU6aVU"
     },
     {
         "name": "Thanh",
@@ -38,6 +40,13 @@ export const sample_users_login = [
         "name": "Thanh123",
         "id": "",
         "email": "Thanh123@gmail.com",
+        "password": "123",
+        "isAdmin": false
+    },
+    {
+        "name": "Viet12345",
+        "id": "",
+        "email": "Viet12345@gmail.com",
         "password": "123",
         "isAdmin": false
     }
